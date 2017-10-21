388c44b5c5c81e0bcf1fe98aea852ced71ac97fb
deathst <jzcxw13@gmail.com>
2017-10-22 07:14:44 +0800

    Removed the chance quicksearch/typeahead passes ID into field.
    I should have saw this coming.

--- quicksearch.php
+++ quicksearch.php
@@ -8,13 +8,14 @@ $mysqli = new mysqli("47.74.176.6", "root", "foobar123!", "GodsEye");
  */
 
 $mysqli = new mysqli("47.74.176.6", "root", "foobar123!", "GodsEye");
-$sql = "SELECT location.name FROM location 
-			WHERE name LIKE '%".$_GET['query']."%'
+$sql = "SELECT location.id, location.name FROM location 
+			WHERE location.name LIKE '%".$_GET['query']."%'
 			LIMIT 10";
 $result = $mysqli->query($sql);
 
 $json = [];
 while($row = $result->fetch_assoc()){
+    //$json[] = $row['id'];
     $json[] = $row['name'];
 }
 echo json_encode($json);
